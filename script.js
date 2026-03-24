5d495b3f33ce4d4b7ddca91e59a7fe54ba7c2974
eafranklin3991 <eafranklin3991@gmail.com>
2026-03-24 16:42:58 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const keypad = document.querySelector(".keypad");
+
+// Create keypad buttons
+for (let i = 0; i < 4; i++) { // cols
+    for (let j = 0; j < 3; j++) { // rows
+    const button = document.createElement("div");
+    button.classList.add("button");
+    button.dataset.row = j;
+    button.dataset.col = i;
+
+    keypad.appendChild(button);
+    }
+}
