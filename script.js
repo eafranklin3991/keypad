ef90a4ba04d172669709717d61704a0214462403
eafranklin3991 <eafranklin3991@gmail.com>
2026-03-24 17:08:23 -0400

    FIx row/col numbering

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const keypad = document.querySelector(".keypad");
 
 // Create keypad buttons
-for (let i = 0; i < 4; i++) { // cols
-    for (let j = 0; j < 3; j++) { // rows
+for (let i = 0; i < 4; i++) { // rows
+    for (let j = 0; j < 3; j++) { // cols
     const button = document.createElement("div");
     button.classList.add("button");
-    button.dataset.row = j;
-    button.dataset.col = i;
+    button.dataset.col = j;
+    button.dataset.row = i;
 
     button.addEventListener("click", () => {
         button.classList.toggle("active");
